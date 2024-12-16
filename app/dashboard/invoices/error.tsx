46248f0231a76050429dfbc3aceb7823baa9ac82
sjmitch-git <sjmitch20@outlook.com>
2024-12-16 19:00:13 +0000

    Completes course

---
+++ app/dashboard/invoices/error.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string; environmentName: string };
+  reset: () => void;
+}) {
+  return (
+    <main className="flex h-full flex-col items-center justify-center">
+      <h2 className="text-center text-red-600 font-semibold mb-4">Something went wrong!</h2>
+      <h3 className="text-center">
+        {error.environmentName} Error: {error.message}
+      </h3>
+      <button
+        className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
+        onClick={
+          // Attempt to recover by trying to re-render the invoices route
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </main>
+  );
+}
